7cc235a6b294e4c20698c979505794eef349baf4
James Kroeger <jameskroeger123@gmail.com>
2021-06-22 23:10:19 +1000

    update macd

--- src/macd.rs
+++ src/macd.rs
@@ -1,25 +1,41 @@
+use std::fmt;
 use ta::{indicators::ExponentialMovingAverage as Ema, Next};
 
 #[derive(Clone)]
 pub struct Macd {
-    pub divergence: f64, // difference between signal series and macd series
+    pub macd: f64,
+    pub signal: f64,
+    pub histogram: f64,
     fast_ema: Ema,
     slow_ema: Ema,
-    signal_ema: Ema, // signal series: ema of the macd series
+    signal_ema: Ema,
 }
 
 impl Macd {
     pub fn new(fast_period: usize, slow_period: usize, signal_period: usize) -> Self {
         Self {
-            divergence: 0.0,
-            fast_ema: Ema::new(fast_period).unwrap(),
-            slow_ema: Ema::new(slow_period).unwrap(),
-            signal_ema: Ema::new(signal_period).unwrap(),
+            macd: 0.,
+            signal: 0.,
+            histogram: 0.,
+            fast_ema: Ema::new(fast_period).unwrap_or(Ema::new(12).unwrap()),
+            slow_ema: Ema::new(slow_period).unwrap_or(Ema::new(26).unwrap()),
+            signal_ema: Ema::new(signal_period).unwrap_or(Ema::new(9).unwrap()),
         }
     }
 
     pub fn next(&mut self, val: f64) {
-        let macd = self.slow_ema.next(val) - self.fast_ema.next(val);
-        self.divergence = self.signal_ema.next(macd) - macd;
+        self.macd = self.fast_ema.next(val) - self.slow_ema.next(val);
+        self.signal = self.signal_ema.next(self.macd);
+        self.histogram = self.macd - self.signal;
+    }
+}
+
+impl fmt::Debug for Macd {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "[macd={} signal={} histogram={}]",
+            self.macd, self.signal, self.histogram
+        )
     }
 }
